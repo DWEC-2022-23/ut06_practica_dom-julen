3d4ccb1fbfde62d734bc3b56f0ed23cb90e801c0
Ordenador de clase <julenlopezpanadero@gmail.com>
2023-01-26 10:15:13 +0100

    Eje2

--- js/ejercicio02.js
+++ js/ejercicio02.js
@@ -8,11 +8,13 @@ function inicio(){
         if(aux){
             div.style.backgroundColor="black"
             div.style.color="white"
+            boton.innerHTML="Fondo claro"
             aux=!aux
         }
         else{
             div.style.backgroundColor=""
             div.style.color=""
+            boton.innerHTML="Fondo oscuro"
             aux=!aux
         }
     })
